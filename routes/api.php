02bd98dcc03e2b042dbec42d5a5bc552b9b19581
firmansyah08 <alif.firmansyah80@gmail.com>
2019-06-18 13:50:48 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+	'middleware' => ['api', 'cors'],
+]), function ($router) {
+	// News
+	Route::get('news', 'NewsController@index');
+	Route::get('news/edit/{id}', 'NewsController@edit');
+	Route::post('news', 'NewsController@store');
+	Route::post('news/{id}', 'NewsController@update');
+	Route::get('news/{id}', 'NewsController@destroy');	
+}
