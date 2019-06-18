cebdc3e9932e59b679b74ff43bdc1c26caac5bed
firmansyah08 <alif.firmansyah80@gmail.com>
2019-06-18 12:18:51 +0700

    Alif - Table News

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+    	# code...
+    }
+
+    public function store(Request $request)
+    {
+    	# code...
+    }
+
+    public function update(Request $request, $id)
+    {
+    	# code...
+    }
+
+    public function destroy($id)
+    {
+    	# code...
+    }
+}
